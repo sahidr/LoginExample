4204fd2e431ea4aca19d0588ebef870852015209
sahidr <sahidr93@gmail.com>
2017-05-23 16:06:14 -0400

    Google Maps Integrated with Dummy Camera App

---
+++ app/src/main/java/com/idbcgroup/loginexample/CameraActivity.java
@@ -0,0 +1,52 @@
+package com.idbcgroup.loginexample;
+
+import android.hardware.Camera;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+
+public class CameraActivity extends AppCompatActivity {
+    private Camera mCamera = null;
+    private CameraView mCameraView = null;
+    private ImageButton imgClose;
+    private ImageButton imgCapture;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        imgClose = (ImageButton)findViewById(R.id.imgClose);
+        imgCapture = (ImageButton)findViewById(R.id.imgCapture);
+
+        try{
+            mCamera = Camera.open();//you can use open(int) to use different cameras
+        } catch (Exception e){
+            Log.d("ERROR", "Failed to get camera: " + e.getMessage());
+        }
+
+        if(mCamera != null) {
+            mCameraView = new CameraView(this, mCamera);//create a SurfaceView to show camera data
+            FrameLayout camera_view = (FrameLayout)findViewById(R.id.camera_view);
+            camera_view.addView(mCameraView);//add the SurfaceView to the layout
+        }
+
+        //btn to close the application
+
+        imgClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.exit(0);
+            }
+        });
+
+        imgCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.exit(0);
+            }
+        });
+    }
+}
--- app/src/main/java/com/idbcgroup/loginexample/MapsActivity.java
+++ app/src/main/java/com/idbcgroup/loginexample/MapsActivity.java
@@ -1,29 +1,75 @@
 package com.idbcgroup.loginexample;
 
-import android.support.v4.app.FragmentActivity;
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.MapStyleOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import android.support.v7.app.AppCompatActivity;
+
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private ImageButton profile;
+    private ImageButton camera;
+    //private ImageButton location;
+    private Location myLocation;
+    private LocationManager locationManager;
+    private static final long MIN_TIME = 400;
+    private static final float MIN_DISTANCE = 1000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
 
+
+        profile = (ImageButton) findViewById(R.id.profile);
+        camera = (ImageButton) findViewById(R.id.camera);
+        //location = (ImageButton) findViewById(R.id.location);
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MapsActivity.this, DashboardActivity.class);
+                startActivity(i);
+            }
+        });
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MapsActivity.this, CameraActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
 
     /**
      * Manipulates the map once available.
@@ -34,15 +80,35 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        MapStyleOptions style;
+        style = MapStyleOptions.loadRawResourceStyle(this, R.raw.mapstyle_treepedia);
+        mMap.setMapStyle(style);
         LatLng caracas = new LatLng(10.4806, -66.9036);
-        mMap.setMinZoomPreference(14);
+        mMap.setMinZoomPreference(12);
         mMap.setMaxZoomPreference(22);
-        mMap.addMarker(new MarkerOptions().position(caracas).title("Marker in Caracas"));
+        mMap.addMarker(new MarkerOptions()
+                .position(caracas)
+                .title("Marker in Caracas")
+                .icon(BitmapDescriptorFactory.fromResource(android.R.drawable.btn_star_big_on)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(caracas));
 
     }
+
+
+
 }
