3248aced07689b6ddd5b0447faf3fd10b060e862
sahidr <sahidr93@gmail.com>
2017-05-24 17:38:55 -0400

    Edit profile section added, minor fixes at Dashboard activity

--- app/src/main/java/com/idbcgroup/loginexample/DashboardActivity.java
+++ app/src/main/java/com/idbcgroup/loginexample/DashboardActivity.java
@@ -1,13 +1,21 @@
 package com.idbcgroup.loginexample;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    private ImageButton back;
+    private ImageButton edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +24,23 @@ public class DashboardActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        back = (ImageButton) findViewById(R.id.back);
+        edit = (ImageButton) findViewById(R.id.edit);
 
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(DashboardActivity.this, EditProfile.class);
+                startActivity(i);
+            }
+        });
 
         /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
